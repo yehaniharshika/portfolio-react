8b3ca4697701ad442d042d1c75df9dbed4af7c3b
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-20 09:08:59 +0530

    Created NavBar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import './App.css'
+import { useEffect, useState } from "react";
+
+import {HeroPage} from "./components/HeroPage.tsx";
+import {Skills} from "./components/Skills.tsx";
+import {NavBar} from "./components/NavBar.tsx";
+import {Contact} from "./components/Contact.tsx";
+
+import {Projects} from "./components/Projects.tsx";
+import {About} from "./components/About.tsx";
 
 function App() {
-
+  const [darkMode, setDarkMode] = useState(() => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("darkMode") === "true";
+    }
+    return false;
+  });
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.setItem("darkMode", darkMode.toString());
+  }, [darkMode]);
   return (
-    <>
-
-    </>
+      <div className="bg-gray-50 dark:bg-gray-900 min-h-screen">
+        <NavBar darkMode={darkMode} setDarkMode={setDarkMode} />
+        <HeroPage/>
+        <About/>
+        <Skills/>
+        <Projects/>
+        <Contact/>
+      </div>
   )
 }
 
