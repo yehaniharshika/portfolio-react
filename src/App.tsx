80f6f65db115d686208c4a69ca2a2516c0427185
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-03-22 16:32:18 +0530

    completed changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-
 import {HeroPage} from "./components/HeroPage.tsx";
 import {Skills} from "./components/Skills.tsx";
 import {NavBar} from "./components/NavBar.tsx";
@@ -10,23 +8,10 @@ import {About} from "./components/About.tsx";
 import {Education} from "./components/Education.tsx";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(() => {
-    if (typeof window !== "undefined") {
-      return localStorage.getItem("darkMode") === "true";
-    }
-    return false;
-  });
-  useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-    localStorage.setItem("darkMode", darkMode.toString());
-  }, [darkMode]);
+
   return (
       <div className="bg-gray-50 dark:bg-gray-900 min-h-screen">
-        <NavBar darkMode={darkMode} setDarkMode={setDarkMode} />
+        <NavBar/>
         <HeroPage/>
         <About/>
         <Education/>
